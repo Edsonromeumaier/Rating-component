b2911272b46f3c1d8da7d62005246cef92418672
Romeu maier <romeumaier@gmail.com>
2023-06-14 13:32:00 -0300

    first state

---
+++ design/script.js
@@ -0,0 +1,10 @@
+var btn = document.getElementsByClassName('btn');
+var escolha = document.querySelector('.evaluation');
+btn.addEventListener("click", function(){
+    if(escolha.getElementsByClassName.display === 'block'){
+        escolha.getElementsByClassName.display = 'none';
+    } else {
+        
+    }
+
+};
