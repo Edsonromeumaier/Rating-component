347df53ee2eac2ae42eaadd9c5d3adda1248be09
Romeu maier <romeumaier@gmail.com>
2023-06-25 15:30:47 -0300

    melhora o script

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-const modal= document.querySelector(".thankYu");
-
+const modal = document.querySelector(".thankYu");
 const openModalBtn = document.querySelector(".btn");
-
 const closeModalBtn = document.querySelector(".btnClose");
- 
 const cardElement = document.querySelector(".card");
-
 const form = document.querySelector('form');
+const ratingElement = document.getElementById('rating');
 
-const ratingElement = document.getElementsByClassName('rating');
-
-
-openModalBtn.addEventListener("click",() => {
-    cardElement.style.display = "none";
-    modal.show();
+openModalBtn.addEventListener("click", () => {
+  cardElement.style.display = "none";
+  modal.showModal();
 });
 
-closeModalBtn.addEventListener("click",()=> { 
-    modal.close();
-    cardElement.style.display = "block";
+closeModalBtn.addEventListener("click", () => {
+  modal.close();
+  cardElement.style.display = "block";
 });
 
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  var selected = document.querySelector('input[name="rating"]:checked');
+
+  if (selected) {
+    var value = selected.value+" ";
+    ratingElement.textContent = value;
+  } else {
+    ratingElement.textContent = 'Nenhuma opção selecionada';
+  }
+
+  modal.showModal();
+});
